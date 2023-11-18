6ade126b0f68dfda751256c77c114cd147cc0ee0
Filip <filip.czekajlo@mixed.world>
2023-11-18 15:39:48 +0100

    CleanUp

--- UGSModule/InventorySystem.cs
+++ UGSModule/InventorySystem.cs
@@ -14,39 +14,31 @@ public class InventorySystem
 
         if (agent != null)
         {
-            await SetAgent(ctx, apiClient, element, agent);
+            return agent;
         }
-        else
+
+        string url = "";
+        switch (element)
         {
-            string url = "";
-            switch (element)
-            {
-                case "Air":
-                    url = "https://raw.githubusercontent.com/filipczekajlo/ALOTA-public/main/DefaultAgentAir.txt";
-                    break;
-                case "Earth":
-                    url = "https://raw.githubusercontent.com/filipczekajlo/ALOTA-public/main/DefaultAgentEarth.txt";
-                    break;
-                case "Fire":
-                    url = "https://raw.githubusercontent.com/filipczekajlo/ALOTA-public/main/DefaultAgentFire.txt";
-                    break;
-                case "Water":
-                    url = "https://raw.githubusercontent.com/filipczekajlo/ALOTA-public/main/DefaultAgentWater.txt";
-                    break;
-            }
-            var DefaultAgent = await DownloadFile(url);
-            
-            await SetAgent(ctx, apiClient, element, DefaultAgent);
-            
-            return DefaultAgent;
+            case "Air":
+                url = "https://raw.githubusercontent.com/filipczekajlo/ALOTA-public/main/DefaultAgentAir.txt";
+                break;
+            case "Earth":
+                url = "https://raw.githubusercontent.com/filipczekajlo/ALOTA-public/main/DefaultAgentEarth.txt";
+                break;
+            case "Fire":
+                url = "https://raw.githubusercontent.com/filipczekajlo/ALOTA-public/main/DefaultAgentFire.txt";
+                break;
+            case "Water":
+                url = "https://raw.githubusercontent.com/filipczekajlo/ALOTA-public/main/DefaultAgentWater.txt";
+                break;
         }
-
-
-        // var agentString = "";
-
-        // var serializedAgent = JsonSerializer.Serialize(agent);
-
-         return "";
+        
+        var DefaultAgent = await DownloadFile(url);
+            
+        await SetAgent(ctx, apiClient, element, DefaultAgent);
+            
+        return DefaultAgent;
     }
 
     private async Task<string?> GetAgent(IExecutionContext ctx, IGameApiClient apiClient, string element)
@@ -60,7 +52,6 @@ public class InventorySystem
 
         if (result.Data.Results.Count == 0) return null;
         return result.Data.Results.First().Value.ToString();
-        // return JsonSerializer.Deserialize<Agent>(result.Data.Results.First().Value.ToString());
     }
 
     private async Task<string> SetAgent(IExecutionContext ctx, IGameApiClient apiClient, string key, string value)
@@ -68,7 +59,7 @@ public class InventorySystem
         var result = await apiClient.CloudSaveData
             .SetItemAsync(ctx, ctx.AccessToken, ctx.ProjectId, ctx.PlayerId, new SetItemBody(key, value));
 
-        return result.Data.ToJson();
+        return result.Data.ToString();
     }
 
     public static async Task<string> DownloadFile(string fileUrl)
